7e0e0d64279a82734f7bedb73e83c2abbe440c69
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2017-10-12 15:28:11 -0500

    created navbar factory

---
+++ nav-factory.js
@@ -0,0 +1,26 @@
+//created nav bar object
+const home = {
+    'name' : 'Home',
+    'link' : "./index.html"
+}
+
+const products = {
+    'name' : 'Producs',
+    'link' : "./index.html"
+}
+
+const contact = {
+    'name' : 'contact',
+    'link' : "./contact.html"
+}
+
+const employees = {
+    'name' : 'employees',
+    'link' : "./employees.html"
+}
+
+//create blank nav bar array
+const navBar = [];
+
+//create array of objects into navBar
+navBar.push(home, products, contact, employees);
