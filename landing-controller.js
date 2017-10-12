ea85a7bdf1b4a15615f7cfa00eb91e0b9dd1a066
Paul Ellis <tynesellis@gmail.com>
2017-10-11 19:50:22 -0500

    DOM updated with random selection from DB each time page refreshes

--- landing-controller.js
+++ landing-controller.js
@@ -1,17 +1,26 @@
 const retrievedPicsDB = JSON.parse(localStorage.getItem("picsDataBase")); //get the data stored in browser
 let landingHTML = document.getElementById("landing"); //target area to send HTML
 
-for (let arr in retrievedPicsDB) {
-    let currentArray = retrievedPicsDB[arr]
-    for (var obj = 0; obj < currentArray.length; obj++) {
-        var currentObject = currentArray[obj];
-        landingHTML.innerHTML += `
-        <section class="imageCard">
-            <h3 class="picsH1">${currentObject.animal}</h3>
-            <img class="celebImg" src="${currentObject.picURL}" alt="${currentObject.alt}" height="100" width="150">
-            <figcaption class="celebCaption">${currentObject.caption}</figcaption>
+// for (let arr in retrievedPicsDB) {
+//     let currentArray = retrievedPicsDB[arr]
+//     for (var obj = 0; obj < currentArray.length; obj++) {
+//         var currentObject = currentArray[obj];
+//         landingHTML.innerHTML += `
+//         <section class="imageCard">
+//             <h3 class="picsH1">${currentObject.animal}</h3>
+//             <img class="celebImg" src="${currentObject.picURL}" alt="${currentObject.alt}" height="100" width="150">
+//             <figcaption class="celebCaption">${currentObject.caption}</figcaption>
 
-        </section>
-        `
-    }
-}
+//         </section>
+//         `
+//     }
+// }
+let randomObject = retrievedPicsDB.pics[Math.ceil((Math.random() * retrievedPicsDB.pics.length) - 1)];//returnes one of the three image objects when the page is refreshed
+landingHTML.innerHTML += `
+        <section class="imageCard">
+           <h3 class="picsH1">${randomObject.animal}</h3>
+           <img class="celebImg" src="${randomObject.picURL}" alt="${randomObject.alt}" height="100" width="150">
+           <figcaption class="celebCaption">${randomObject.caption}</figcaption>
+
+       </section>
+         `;
